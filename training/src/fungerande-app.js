51a8520e57f985d1033d3620d2c7c225d514daa2
atacankais <atacankais@gmail.com>
2024-09-10 23:54:18 +0200

    first commit

---
+++ training/src/fungerande-app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h2>Are you Melee or Ranged?</h2>
+      <button>Melee</button>
+      <button>Ranged</button>
+      <h2>Is your enemy Melee or Ranged?</h2>
+      <button>Melee</button>
+      <button>Ranged</button>
+      <button>Ranged(Poke)</button><br /><br />
+      <button>Generera tips</button>
+    </div>
+  );
+}
+
+export default App;
